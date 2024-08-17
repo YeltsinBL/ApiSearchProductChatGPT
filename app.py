fec869acec576be2b4d21a4a0165a3e42a255395
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-08-17 18:34:59 -0500

    Agregar Swagger y modificar APIs de Productos

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
+from flask_swagger_ui import get_swaggerui_blueprint
 
 load_dotenv()
 
@@ -16,25 +17,11 @@ app.config["MYSQL_CURSORCLASS"] = os.environ['MYSQL_CURSORCLASS']
 
 mysql=MySQL(app)
 
-productos=[
-    {
-        "productID":1,
-        "nombre":"Shampoo"
-    },
-    {
-        "productID":2,
-        "nombre":"Jabón"
-    },
-    {
-        "productID":3,
-        "nombre":"Pasta"
-    }
-]
 
-@app.route('/', methods=['GET'])
-def home():
-    """Principal"""
-    return jsonify(productos)
+# @app.route('/', methods=['GET'])
+# def home():
+#     """Principal"""
+#     return jsonify(productos)
 @app.route('/category', methods=['GET'])
 def category():
     """Categoria"""
@@ -69,31 +56,32 @@ def products():
     try:
         add_where =""
         if request.args:
-            add_where="where "
+            print(request.args)
+            # add_where="where "
             if request.args.get('nombre'):
-                if len(add_where)>6: add_where += "and "
-                add_where += "nombre like '%{0}%' ".format(request.args.get('nombre'))
+                add_where += "where nombre like '%{0}%' ".format(request.args.get('nombre'))
             if request.args.get('precio_min'):
-                if len(add_where)>6: add_where += "and "
+                add_where += "where " if len(add_where)==0 else "and "
                 add_where += "precio>={0} ".format(float(request.args.get('precio_min')))
             if request.args.get('precio_max'):
-                if len(add_where)>6: add_where += "and "
+                add_where += "where " if len(add_where)==0 else "and "
                 add_where += "precio<{0} ".format(float(request.args.get('precio_max')))
             if request.args.get('categoria_id'):
-                if len(add_where)>6: add_where += "and "
+                add_where += "where " if len(add_where)==0 else "and "
                 add_where += "categoria_id={0} ".format(int(request.args.get('categoria_id')))
             if request.args.get('marca_id'):
-                if len(add_where)>6: add_where += "and "
+                add_where += "where " if len(add_where)==0 else "and "
                 add_where += "marca_id={0} ".format(int(request.args.get('marca_id')))
-            if request.args.get('en_oferta'):
-                if len(add_where)>6: add_where += "and "
+            if request.args.get('en_oferta') and request.args.get('en_oferta')=='true':
+                add_where += "where " if len(add_where)==0 else "and "
                 add_where += "en_oferta=1 "
                 add_where += "and descuento>0 "
             if request.args.get('popularidad'):
                 add_where += "order by popularidad desc"
-                print(request.args.get('popularidad'))
+                #print(request.args.get('popularidad'))
         cur = mysql.connection.cursor()
-        sql_consulta ="""select id, nombre, descripcion, precio, stock, en_oferta, descuento, popularidad from ecommerce.productos """+add_where
+        sql_consulta ="""select id, nombre, descripcion, precio, stock, en_oferta,\
+                descuento, popularidad from ecommerce.productos """+add_where
         print(sql_consulta)
         cur.execute(sql_consulta)
         result = cur.fetchall()
@@ -105,19 +93,20 @@ def products():
     except Exception as ex:
         return "Error"
 
-@app.route('/product/<id>', methods=['GET'])
-def product_by_id(id):
+@app.route('/product/<productId>', methods=['GET'])
+def product_by_id(productId):
     """Principal"""
     try:
         cur = mysql.connection.cursor()
-        cur.execute("""select id, nombre, descripcion, precio, stock\
-                     from ecommerce.productos where id='{0}'""".format(int(id)))
+        cur.execute("""select id, nombre, descripcion, precio, stock, en_oferta, descuento, popularidad\
+                     from ecommerce.productos where id='{0}'""".format(int(productId)))
         result = cur.fetchall()
-        productos=[]
+        # print(result)
+        producto={}
         for fila in result:
-            productos.append(fila)
-            print(fila)
-        return jsonify(productos)
+            producto=fila
+            print(producto)
+        return jsonify(producto)
     except Exception as ex:
         return "Error"
 
@@ -125,6 +114,19 @@ def pagina_no_encontrada(error):
     """Página"""
     # return jsonify({"error":404, "mensaje":"Ruta no encontrada"})
     return "<h1>Página no encontrada</h1>"
+
+# Configurar Swagger
+SWAGGER_URL = ''
+API_URL = '/static/swagger.json'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={ 
+        'app_name': "Api Ecommerce Search Products"
+    }
+)
+app.register_blueprint(swaggerui_blueprint)
 
 if __name__ == '__main__':
     app.register_error_handler(404, pagina_no_encontrada)
